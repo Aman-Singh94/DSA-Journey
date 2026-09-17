a05c07db09bb6395c898ae9429dd94ab8fc2af9a
Aman Singh <as5645512@gmail.com>
2026-09-17 22:45:11 +0530

    3

---
+++ 3.py
@@ -0,0 +1,16 @@
+#Check if a Number is Palindrome or Not
+num = int(input("Enter a number: "))
+
+original = num
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+if original == reverse:
+    print("Palindrome Number")
+else:
+    print("Not a Palindrome Number")
+
